a76759b67d1b326dd9518412f69deb64f0a29509
VELAT TASKIRAN <velattaskiran@gmail.com>
2024-08-28 22:19:06 +0300

    first commit

---
+++ src/main/java/com/taskiranvelat/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,27 @@
+package com.taskiranvelat.springboot.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.taskiranvelat.springboot.learn_jpa_and_hibernate.course.Course;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+
+    @Autowired
+    private CourseJdbcRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert(new Course(1, "Learn AWS Now!", "taskiran"));
+        repository.insert(new Course(2, "Learn Azure Now!", "taskiran"));
+        repository.insert(new Course(3, "Learn DevOps Now!", "taskiran"));
+
+        repository.deleteById(1);
+
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
+    }
+
+}
